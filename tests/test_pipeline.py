48ad391951a2049ce8b0bd0c41eafd61ed8b6b20
joncrall <jon.crall@kitware.com>
2026-02-04 14:00:23 -0500

    tweak test

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -8,15 +8,15 @@ def test_variable_inputs():
     from kwdagger.pipeline import Pipeline
 
     # A simple pipeline where we don't need to manage reconfiguration.
-    node1 = ProcessNode(name='node1', executable='node1', out_paths={'key1': 'path1'}, node_dpath='.')
-    node2 = ProcessNode(name='node2', executable='node2', out_paths={'key2': 'path2'}, node_dpath='.')
-    node3 = ProcessNode(name='node3', executable='node3', out_paths={'key3': 'path3'}, node_dpath='.')
+    node1 = ProcessNode(name='node1', executable='node1', out_paths={'output': 'path1'}, node_dpath='.')
+    node2 = ProcessNode(name='node2', executable='node2', out_paths={'output': 'path2'}, node_dpath='.')
+    node3 = ProcessNode(name='node3', executable='node3', out_paths={'output': 'path3'}, node_dpath='.')
 
     combine_node = ProcessNode(name='combine', executable='combine', in_paths={'varpaths'})
 
-    node1.outputs['key1'].connect(combine_node.inputs['varpaths'])
-    node2.outputs['key2'].connect(combine_node.inputs['varpaths'])
-    node3.outputs['key3'].connect(combine_node.inputs['varpaths'])
+    node1.outputs['output'].connect(combine_node.inputs['varpaths'])
+    node2.outputs['output'].connect(combine_node.inputs['varpaths'])
+    node3.outputs['output'].connect(combine_node.inputs['varpaths'])
 
     dag_nodes = [
         node1,
@@ -32,6 +32,9 @@ def test_variable_inputs():
     print(node2.final_command())
     print(node3.final_command())
     print(combine_node.final_command())
+
+    print('---')
+    dag.print_commands(with_status=0, with_guards=0, with_locks=0)
 
     pred_nodes = combine_node.predecessor_process_nodes()
     assert list(pred_nodes) == [node1, node2, node3]
